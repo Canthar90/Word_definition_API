2d60645fe3499c85fddf314783ef24aa51aff105
Canthar90 <d.cieslak90@gmail.com>
2023-01-03 22:50:23 +0100

    Added working api endpoint

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, jsonify
 from flask_bootstrap import Bootstrap
+import pandas as pd
 
 
 app = Flask(__name__)
@@ -12,9 +13,16 @@ def home():
 
 @app.route("/api/v1/<word>")
 def definition_ednpoint(word):
-    output = {"definition": word.upper(),
-              "word": word}
-    return output
+    try:
+        df = pd.read_csv("dictionary.csv", encoding="utf-8")
+        definition = df.loc[df['word'] == word.lower()]["definition"].squeeze()
+        output = {"definition": definition,
+                "word": word}
+        return jsonify(output)
+    except:
+        return jsonify(response={"error": "Argument's passed are invalid"}), 404
+    
+    
 
 
 if __name__ == "__main__":
